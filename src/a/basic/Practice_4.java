06f3aa2c4f886a473695393c38fc319089b26cfd
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-03 17:06:22 +0900

    commit

--- src/a/basic/Practice_4.java
+++ src/a/basic/Practice_4.java
@@ -1,5 +1,21 @@
 package a.basic;
 
-public class Practice_4 {
+import java.util.Scanner;
 
+public class Practice_4 {
+    public static void main(String[] args) {
+        //int a = 3, b = 4, c = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a변의 길이 입력: ");
+        int a = sc.nextInt();
+        System.out.print("b변의 길이 입력: ");
+        int b = sc.nextInt();
+        System.out.print("c변의 길이 입력: ");
+        int c = sc.nextInt();
+
+        boolean valid = (a>0&&b>0&&c>0) && ((a+b)>c&&(b+c)>a&&(a+c)>b);
+        if (valid) {
+            System.out.println("삼각형을 만들 수 있습니다");
+        } else System.out.println("삼각형을 만들 수 없습니다");
+    }
 }
---
+++ src/a/basic/Practice_5.java
@@ -0,0 +1,7 @@
+package a.basic;
+
+public class Practice_5 {
+    public static void main(String[] args) {
+
+    }
+}
