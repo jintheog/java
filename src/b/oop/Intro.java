2096c40a76320f24977b328b463030b046807d55
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-05 13:58:00 +0900

    commit

--- src/b/oop/Intro.java
+++ src/b/oop/Intro.java
@@ -3,5 +3,13 @@ package b.oop;
 public class Intro {
     public static void main(String[] args) {
 
+        int rec1 = calArea(10,10);
+        int rec2 = calArea(20,20);
+        int rec3 = calArea(30,30);
+
+        System.out.println(rec1);
+    }
+    public static int calArea(int width, int height) {
+        return width * height;
     }
 }
---
+++ src/b/oop/Main.java
@@ -0,0 +1,9 @@
+package b.oop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle(10, 20);
+
+    }
+}
