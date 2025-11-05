41c0ea98c7926de5d7cde897236d89c01ef082ff
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-05 14:16:23 +0900

    commit

--- src/b/oop/Main.java
+++ src/b/oop/Main.java
@@ -5,5 +5,30 @@ public class Main {
 
         Rectangle r1 = new Rectangle(10, 20);
 
+        Rectangle r2 = new Rectangle(20, 20);
+        Rectangle r3 = new Rectangle(100, 100);
+
+        System.out.println(r1);
+        System.out.println(r2);
+
+        System.out.println(r1.width);
+        System.out.println(r1.area());
+
+        Circle c1 = new Circle(10);
+        Circle c2 = new Circle(20);
+
+        Circle c3 = c2;
+
+        System.out.println(c1.radius);
+        System.out.println(c2.radius);
+        System.out.println(c3.radius);
+
+        c1.radius = 100;
+        c2.radius = 200;
+
+        System.out.println(c1.radius);
+        System.out.println(c2.radius);
+        System.out.println(c3.radius);
+
     }
 }
