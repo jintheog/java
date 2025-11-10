fda23ac1cdfe7a0ea26f5d8766d07936da67a46e
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-10 10:49:33 +0900

    commit

---
+++ src/d/inheritance/OverridingMain.java
@@ -0,0 +1,22 @@
+package d.inheritance;
+
+class Parent {
+    void show() {
+        System.out.println("parent!");
+    }
+}
+
+class Child extends Parent {
+
+    @Override
+    void show() {
+        System.out.println("child!");
+    }
+}
+
+public class OverridingMain {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.show();
+    }
+}
--- src/d/inheritance/SuperMain.java
+++ src/d/inheritance/SuperMain.java
@@ -1,4 +1,63 @@
 package d.inheritance;
 
+class Animal2 {
+    String name;
+    int age;
+    String breed;
+
+    // Animal2(){}
+    Animal2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void introduce() {
+        System.out.println("hello "+ this.name + this.age);
+    }
+
+}
+class Dog2 extends Animal2 {
+    String breed;
+
+    // 생성자 메서드가 없는경우?
+    Dog2() {
+        super("name", 0);
+        // super는 Animal2("name","age")를 호출
+    }
+    Dog2(String name, int age, String breed) {
+        super(name, age);
+        //super는
+        //Animal2(name, age)
+        //this.name = name;
+        //this.age = age;
+        this.breed = breed;
+    }
+
+    void display() {
+        System.out.println(this.breed);
+        System.out.println(super.breed);
+        System.out.println(breed);
+    }
+
+    void introduce(){
+        super.introduce(); // Animal2.introduce(); 와 같음
+        System.out.println("멍멍");
+    }
+}
+
+
 public class SuperMain {
+    public static void main(String[] args) {
+        Animal2 a  = new Animal2("동물", 0);
+        Dog2 d = new Dog2("초코", 3, "말티즈");
+
+        System.out.println(a.breed);
+        System.out.println(d.breed);
+
+        d.display();
+
+        d.introduce();
+        a.introduce();
+    }
+
 }
